f3b8e085d552cdf9fa4b59bf4b80333160ee1461
MatheusBrandao525 <mafe123silva@gmail.com>
2023-01-03 11:21:50 -0300

    Projeto Finalizado

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import styled from "styled-components";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { isStyledComponent } from "styled-components";
-
+import Form from "./components/Form.js";
+import Grid from "./components/Grid.js";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Container = styled.div`
   width: 100%;
@@ -18,10 +21,28 @@ const Container = styled.div`
 const Title = styled.div``;
 
 function App() {
+const [users, setUsers] = useState([]);
+const [onEdit, setOnEdit] = useState(null);
+
+const getUsers = async() => {
+  try {
+    const res = await axios.get("http://localhost:8800");
+    setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+  }catch (error) {
+    toast.error(error);
+  }
+};
+
+useEffect(() => {
+  getUsers();
+}, [setUsers]);
+
   return (
     <>
       <Container>
         <Title>USUÁRIOS</Title>
+        <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers}/>
+        <Grid users={users} setUsers={setUsers} setOnEdit={setOnEdit}/>
       </Container>
       <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT}/>
       <GlobalStyle />
--- src/components/form.js
+++ src/components/form.js
@@ -1,6 +1,7 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
-
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const FormContainer = styled.form`
     display: flex; 
@@ -28,15 +29,92 @@ const Input = styled.input`
 
 const Label = styled.label``;
 
-const Form = ({ onEdit }) => {
+const Button = styled.button`
+    padding: 10;
+    cursor: pointer;
+    border-radius: 5px;
+    border: none;
+    background-color: #2c73d2;
+    color: white;
+    height: 42px;
+`;
+
+const Form = ({ getUsers, onEdit, setOnEdit }) => {
 
     const ref = useRef();
+
+    useEffect(() => {
+        if(onEdit) {
+            const user = ref.current;
+
+            user.nome.value = onEdit.nome;
+            user.email.value = onEdit.email;
+            user.fone.value = onEdit.fone;
+            user.data_nascimento.value = onEdit.data_nascimento;
+        }
+    }, [onEdit]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const user = ref.current;
+
+        if (
+            !user.nome.value || !user.email.value || !user.fone.value || !user.data_nascimento.value
+        ) {
+            return toast.warn("Preencha todos os campos");
+        }
+
+        if (onEdit) {
+            await axios.put("http://localhost:8800/" + onEdit.id, {
+                nome: user.nome.value,
+                email: user.email.value,
+                fone: user.fone.value,
+                data_nascimento: user.data_nascimento.value,
+            })
+            .then(({data}) => toast.success(data))
+            .catch(({data}) => toast.error(data));
+        } else {
+            await axios.post("http://localhost:8800/", {
+                nome: user.nome.value,
+                email: user.email.value,
+                fone: user.fone.value,
+                data_nascimento: user.data_nascimento.value,
+            })
+            .then(({data}) => toast.success(data))
+            .catch(({data}) => toast.error(data));
+        }
+
+        user.nome.value = "";
+        user.email.value = "";
+        user.fone.value = "";
+        user.data_nascimento.value = "";
+
+        setOnEdit(null);
+        getUsers();
+    };
+
     return (
-        <FormContainer ref={ref}>
+        <FormContainer ref={ref} onSubmit={handleSubmit}>
             <InputArea>
                 <Label>Nome</Label>
                 <Input name="nome" />
             </InputArea>
+            <InputArea>
+                <Label>E-mail</Label>
+                <Input name="email" type="email"/>
+            </InputArea>
+            <InputArea>
+                <Label>fone</Label>
+                <Input name="fone" />
+            </InputArea>
+            <InputArea>
+                <Label>Data de nascimento</Label>
+                <Input name="data_nascimento" type="date" />
+            </InputArea>
+            <Button type="submit">SALVAR</Button>
         </FormContainer>
     );
 };
+
+export default Form;
